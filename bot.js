6dfdc85473d9b80fbdaa25a739e8002439bb77de
Ahmid <Ahmad-hjz@hotmail.com>
2017-02-25 00:17:28 +0200

    initial commit

---
+++ bot.js
@@ -0,0 +1,11 @@
+const TeleBot = require('telebot');
+const bot = new TeleBot('372091526:AAHdKP9UQsi8FTBqsmJ0P2hX4BdnkUTIEG0');
+
+bot.on('text', msg => {
+  let fromId = msg.from.id;
+  let firstName = msg.from.first_name;
+  let reply = msg.message_id;
+  return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
+});
+
+bot.connect();
