2cab4e751193ca047be8b374aebc58aaf3cc229f
Ahmid <Ahmad-hjz@hotmail.com>
2017-02-25 00:54:54 +0200

    new

--- bot.js
+++ bot.js
@@ -2,9 +2,15 @@ var express = require ('express');
 var app = express();
 var PORT = process.env.PORT || 3005;
 
-app.get('/', function (req,res){
-    res.send('Hello!!!');
+var middleware = require ('./middleware.js');
+
+app.use (middleware.logger);
+
+app.get('/about',middleware.requireAuthentication ,function (req, res) {
+    res.send('about us!!');
 });
+
+app.use(express.static(__dirname + "/public"));
 
 // const TeleBot = require('telebot');
 // const bot = new TeleBot('372091526:AAHdKP9UQsi8FTBqsmJ0P2hX4BdnkUTIEG0');
@@ -16,9 +22,6 @@ app.get('/', function (req,res){
 //   return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
 // });
 // bot.connect();
-
-
-app.use(express.static(__dirname + "/public"));
 
 app.listen(PORT, function () {
     console.log ('Express server started on port ' + PORT + '!');
