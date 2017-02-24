a9f959f1ca539823bce0b87eab22928a5502d9ea
Ahmid <Ahmad-hjz@hotmail.com>
2017-02-25 00:49:06 +0200

    add express

--- bot.js
+++ bot.js
@@ -6,16 +6,16 @@ app.get('/', function (req,res){
     res.send('Hello!!!');
 });
 
-const TeleBot = require('telebot');
-const bot = new TeleBot('372091526:AAHdKP9UQsi8FTBqsmJ0P2hX4BdnkUTIEG0');
+// const TeleBot = require('telebot');
+// const bot = new TeleBot('372091526:AAHdKP9UQsi8FTBqsmJ0P2hX4BdnkUTIEG0');
 
-bot.on('text', msg => {
-  let fromId = msg.from.id;
-  let firstName = msg.from.first_name;
-  let reply = msg.message_id;
-  return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
-});
+// bot.on('text', msg => {
+//   let fromId = msg.from.id;
+//   let firstName = msg.from.first_name;
+//   let reply = msg.message_id;
+//   return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
+// });
 
 
-bot.connect();
+// bot.connect();
 app.listen(PORT);
