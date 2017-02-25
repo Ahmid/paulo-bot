629c56f269fad65b8fafde1801f92d5a8dbfebfd
Ahmid <Ahmad-hjz@hotmail.com>
2017-02-25 10:58:16 +0200

    package config

--- bot.js
+++ bot.js
@@ -12,16 +12,16 @@ app.get('/about',middleware.requireAuthentication ,function (req, res) {
 
 app.use(express.static(__dirname + "/public"));
 
-// const TeleBot = require('telebot');
-// const bot = new TeleBot('372091526:AAHdKP9UQsi8FTBqsmJ0P2hX4BdnkUTIEG0');
+const TeleBot = require('telebot');
+const bot = new TeleBot('372091526:AAHdKP9UQsi8FTBqsmJ0P2hX4BdnkUTIEG0');
 
-// bot.on('text', msg => {
-//   let fromId = msg.from.id;
-//   let firstName = msg.from.first_name;
-//   let reply = msg.message_id;
-//   return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
-// });
-// bot.connect();
+bot.on('text', msg => {
+  let fromId = msg.from.id;
+  let firstName = msg.from.first_name;
+  let reply = msg.message_id;
+  return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
+});
+bot.connect();
 
 app.listen(PORT, function () {
     console.log ('Express server started on port ' + PORT + '!');
