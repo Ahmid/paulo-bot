03d3890398b13fcb705bcf228aa7fd97f330744f
Ahmid <Ahmad-hjz@hotmail.com>
2017-02-25 16:57:54 +0200

    new

--- bot.js
+++ bot.js
@@ -1,38 +1,106 @@
 var express = require('express');
-var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
 const TeleBot = require('telebot');
-const bot = new TeleBot('372091526:AAHdKP9UQsi8FTBqsmJ0P2hX4BdnkUTIEG0');
+const bot = new TeleBot('305749132:AAF7gJgtnYp4lV4K1cRZ4ANv5eA6xe3rHjs');
 
-var quotes = [];
+var PauloQuotes = [];
+var KafkaQuotes = [];
+var date = new Date();
+var month = date.getUTCMonth();
 
-url = 'https://www.goodreads.com/author/quotes/566.Paulo_Coelho?page=1';
-request(url, function(error, response, html) {
-    if(!error) {
-        var $ = cheerio.load(html);
 
-        $('.quotes .quote .quoteDetails .quoteText').each (function (i, elm) {
+//Paulo
+function FillPauloArray (page) {
+    url = 'https://www.goodreads.com/author/quotes/566.Paulo_Coelho?page=' + page;
+    request(url, function(error, response, html) {
+        if(!error) {
+            var $ = cheerio.load(html);
 
-            var data = $(this).first().contents().filter (function () {
-                 return this.type === 'text';
-            }).text().replace(/[^a-zA-Z ]/g, "").trim();
+            $('.quotes .quote .quoteDetails .quoteText').each (function (i, elm) {
 
-            quotes.push(data + ".");
-        });
-    }
+                var data = $(this).first().contents().filter (function () {
+                    return this.type === 'text';
+                }).text().replace(/[^a-zA-Z ]/g, "").trim();
+
+                PauloQuotes.push(data + ".");
+            });
+        }
+    });
+}
+
+//Kafka
+function FillKafkaArray (page) {
+    url = 'https://www.goodreads.com/author/quotes/5223.Franz_Kafka?page=' + page;
+    request(url, function(error, response, html) {
+        if(!error) {
+            var $ = cheerio.load(html);
+
+            $('.quotes .quote .quoteDetails .quoteText').each (function (i, elm) {
+
+                var data = $(this).first().contents().filter (function () {
+                    return this.type === 'text';
+                }).text().replace(/[^a-zA-Z ]/g, "").trim();
+
+                KafkaQuotes.push(data + ".");
+            });
+        }
+    });
+}
+
+bot.on(['text','audio','voice','photo'], msg => {
+    let fromId = msg.from.id;
+
+    return bot.sendMessage(fromId, "Type / followed by writer's name to get the daily quote!" + 
+    "\n\nExample:\n/paulo\n/kafka");
 });
 
-bot.on('text', msg => {
+bot.on('/start', msg => {
     let fromId = msg.from.id;
     let firstName = msg.from.first_name;
-    let reply = msg.message_id;
 
-    return bot.sendMessage(fromId, "Today's quote: " + quotes[2], { reply });
+    return bot.sendMessage(fromId, "Hello " + firstName + "! 😊\nHope you will enjoy the daily quotes!");
+});
+
+bot.on(['/paulo','/Paulo','/PAULO'], msg => {
+    let fromId = msg.from.id;
+    let firstName = msg.from.first_name;
+
+    var newDate = new Date();
+    if (newDate.getUTCMonth() > month) {
+        FillPauloArray (newDate.getUTCMonth());
+        month = newDate.getUTCMonth();
+    }
+
+    var day = date.getUTCDate();
+    if (day === 31) {
+        day = 0;
+    }
+    return bot.sendMessage(fromId, "Today's Paulo quote:\n" + PauloQuotes[day]);
+});
+
+bot.on(['/kafka','/Kafka','KAFKA'], msg => {
+    let fromId = msg.from.id;
+    let firstName = msg.from.first_name;
+
+    var newDate = new Date();
+    if (newDate.getUTCMonth() > month) {
+        FillPauloArray (newDate.getUTCMonth());
+        month = newDate.getUTCMonth();
+    }
+
+    var day = date.getUTCDate();
+    if (day === 31) {
+        day = 0;
+    }
+    return bot.sendMessage(fromId, "Today's Kafka quote:\n" + KafkaQuotes[day]);
 });
 bot.connect();
 
+
+FillPauloArray(month);
+FillKafkaArray(month);
 app.listen('3005')
 exports = module.exports = app;
